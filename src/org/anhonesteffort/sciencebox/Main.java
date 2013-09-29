bea633af9063ca26e26a10cf18f12eedae98c465
rhodey <rhodey@anhonesteffort.org>
2013-09-29 00:39:48 -0700

    Small changes to the examples in Main.

--- src/org/anhonesteffort/sciencebox/Main.java
+++ src/org/anhonesteffort/sciencebox/Main.java
@@ -4,6 +4,8 @@ import jssc.SerialPort;
 import jssc.SerialPortException;
 import org.anhonesteffort.sciencebox.hardware.control.Blower;
 import org.anhonesteffort.sciencebox.hardware.control.Humidifier;
+import org.anhonesteffort.sciencebox.hardware.sensor.HumiditySensor;
+import org.anhonesteffort.sciencebox.hardware.sensor.TemperatureSensor;
 import org.anhonesteffort.sciencebox.serial.ScienceSerialServer;
 
 public class Main {
@@ -14,14 +16,18 @@ public class Main {
 
     try {
 
+      // Serial port and server thing.
       sciencePort.openPort();
       sciencePort.setParams(SerialPort.BAUDRATE_9600, SerialPort.DATABITS_8, SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
-
       ScienceSerialServer scienceSerialServer = new ScienceSerialServer(sciencePort);
 
+      // Sensors.
+      TemperatureSensor scienceTemperature = new TemperatureSensor(scienceSerialServer);
+      HumiditySensor humiditySensor = new HumiditySensor(scienceSerialServer);
+
+      // Active hardware.
       Blower scienceBlower = new Blower(scienceSerialServer);
       scienceBlower.on();
-
       Humidifier scienceHumidifier = new Humidifier(scienceSerialServer);
       scienceHumidifier.on();
 
